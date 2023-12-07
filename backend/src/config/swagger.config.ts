5e40e6aba708e1a3b079eefe8e050425bf48ea13
dinaaz <dinaaz.codes@gmail.com>
2023-12-08 01:20:09 +0400

    chore: add swagger doc to health endpoint

--- backend/src/config/swagger.config.ts
+++ backend/src/config/swagger.config.ts
@@ -1,5 +1,9 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
+export const API_TAGS = {
+  health: 'Ping',
+};
+
 export const SWAGGER_CONFIGS = {
   title: 'EasyGenerator Task Api',
   version: '1.0',
@@ -12,5 +16,6 @@ export const getSwaggerConfigurations = () => {
     .setTitle(SWAGGER_CONFIGS.title)
     .setDescription(SWAGGER_CONFIGS.description)
     .setVersion(SWAGGER_CONFIGS.version)
+    .addTag(API_TAGS.health)
     .build();
 };
