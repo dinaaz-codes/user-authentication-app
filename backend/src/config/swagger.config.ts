115859cdf02e916119ec9ea3f47e0389fac17ad5
dinaaz <dinaaz.codes@gmail.com>
2023-12-08 01:19:30 +0400

    chore: set up swagger configs

---
+++ backend/src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const SWAGGER_CONFIGS = {
+  title: 'EasyGenerator Task Api',
+  version: '1.0',
+  description:
+    'Consists of apis responsible for carrying out user sign up and sign in',
+};
+
+export const getSwaggerConfigurations = () => {
+  return new DocumentBuilder()
+    .setTitle(SWAGGER_CONFIGS.title)
+    .setDescription(SWAGGER_CONFIGS.description)
+    .setVersion(SWAGGER_CONFIGS.version)
+    .build();
+};
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { getSwaggerConfigurations } from './config/swagger.config';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
+
+  const document = SwaggerModule.createDocument(
+    app,
+    getSwaggerConfigurations(),
+  );
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
