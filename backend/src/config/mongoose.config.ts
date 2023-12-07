b86c8310281be7cd0496aa903338f556a6bdd0e5
dinaaz <dinaaz.codes@gmail.com>
2023-12-08 02:16:16 +0400

    chore: configure mongoose

---
+++ backend/src/config/mongoose.config.ts
@@ -0,0 +1,21 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+
+export const MongooseConfigs = (
+  configService: ConfigService,
+): MongooseModuleOptions => {
+  const dbLogger = new Logger('MongooseLogger');
+
+  return {
+    uri: <string>configService.get('MONGO_URL'),
+    retryAttempts: 5,
+    connectionFactory: (connection: Connection) => {
+      if (connection.readyState) {
+        dbLogger.log('Mongo database connected');
+      }
+      return connection;
+    },
+  };
+};
