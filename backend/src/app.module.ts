cc92fa5452a54f4b99b3dab2fe6237af36ed64d5
dinaaz <dinaaz.codes@gmail.com>
2023-12-08 01:02:12 +0400

    refactor: remove config module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './config/config.module';
 import { HealthModule } from './health/health.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+      isGlobal: true,
+    }),
+    HealthModule,
+  ],
 })
 export class AppModule {}
