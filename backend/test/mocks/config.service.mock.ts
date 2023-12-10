f402208a4e06576c7587fb8a1f3b15623927c837
dinaaz <dinaaz.codes@gmail.com>
2023-12-11 01:01:01 +0400

    refactor: add remaining keys to config service

--- backend/test/mocks/config.service.mock.ts
+++ backend/test/mocks/config.service.mock.ts
@@ -6,5 +6,28 @@ export class MockConfigService {
     if (key === 'PORT') {
       return 4000;
     }
+    if (key === 'APP_ENV') {
+      return 'test';
+    }
+
+    if (key === 'MONGO_URL') {
+      return undefined;
+    }
+
+    if (key === 'MONGO_TEST_URL') {
+      return 'mongodb+srv://easy-generator-api:8eoC2jwtqZXAeeGj@cluster0.2vaw0l1.mongodb.net?retryWrites=true&w=majority';
+    }
+
+    if (key === 'JWT_SECRET') {
+      return 'testjwt';
+    }
+
+    if (key === 'JWT_RT_SECRET') {
+      return 'testrtjwt';
+    }
+
+    if (key === 'FRONTEND_URL') {
+      return 'http://localhost:3000';
+    }
   }
 }
