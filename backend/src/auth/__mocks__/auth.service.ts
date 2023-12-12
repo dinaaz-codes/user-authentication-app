f718b458492ec843daff03f60d22c132bd9d3b06
dinaaz <dinaaz.codes@gmail.com>
2023-12-13 02:03:38 +0400

    fix: auth service resolved value

--- backend/src/auth/__mocks__/auth.service.ts
+++ backend/src/auth/__mocks__/auth.service.ts
@@ -1,8 +1,8 @@
 import { tokenStub } from '../test/stubs/tokens.stub';
 
 export const AuthService = jest.fn().mockReturnValue({
-  signUp: jest.fn().mockResolvedValue(tokenStub),
-  signIn: jest.fn().mockResolvedValue(tokenStub),
-  refreshTokens: jest.fn().mockResolvedValue(tokenStub),
+  signUp: jest.fn().mockResolvedValue(tokenStub()),
+  signIn: jest.fn().mockResolvedValue(tokenStub()),
+  refreshTokens: jest.fn().mockResolvedValue(tokenStub()),
   signOut: jest.fn().mockResolvedValue(undefined),
 });
